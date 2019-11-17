2786100ceee7e21c9f63db36eb687241ac71c8e7
gilbert <gilbert@savics.org>
2019-11-17 22:29:46 +0100

    add restart containers  script

---
+++ restart-containers.sh
@@ -0,0 +1,6 @@
+docker-compose restart  openmrs-tomcat
+docker-compose restart  openmrs-tomcat-bethsaida-hc-29
+docker-compose restart  openmrs-tomcat-kabusunzu-hc-6
+docker-compose restart  openmrs-tomcat-cor-unum-hc-4
+docker-compose restart  openmrs-tomcat-rugarama-hc-11
+docker-compose restart  openmrs-tomcat-muhima-hc-7
