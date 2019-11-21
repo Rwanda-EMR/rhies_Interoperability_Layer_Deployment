9aeb9ac37ad9bc60743f9f31a2b976b0626240a0
Botics RHIES EMR <botics@EMR.idc.aos.rw>
2019-11-21 09:41:33 +0200

    added new sites We-Act, Clinique-Carrefour

--- restart-containers.sh
+++ restart-containers.sh
@@ -4,3 +4,5 @@ docker-compose restart  openmrs-tomcat-kabusunzu-hc-6
 docker-compose restart  openmrs-tomcat-cor-unum-hc-4
 docker-compose restart  openmrs-tomcat-rugarama-hc-11
 docker-compose restart  openmrs-tomcat-muhima-hc-7
+docker-compose restart  openmrs-tomcat-we-act-1237
+docker-compose restart  openmrs-tomcat-clinique-carrefour-657
